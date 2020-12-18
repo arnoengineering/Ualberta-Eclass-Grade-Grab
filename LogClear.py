09bc2a19fefeffc848d2dc4b311d17616836c44c
Arno Claassens <63562772+arnoengineering@users.noreply.github.com>
2020-12-18 09:51:10 -0700

    home
    Signed-off-by: Arno Claassens <63562772+arnoengineering@users.noreply.github.com>

--- LogClear.py
+++ LogClear.py
@@ -2,7 +2,10 @@
 # Removes log file and sends old file if errors
 import smtplib
 import ssl
-from email.message import EmailMessage
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import logging
 
 usr_f = open('UsrInfo.txt', 'r')
@@ -21,13 +24,14 @@ with open(fname, 'r') as f:
     for line in f:
         c += 1
 
+attachment = [fname, 'Grades.csv']
 logging.basicConfig(filename=fname, level=logging.INFO)
 
 host_server = "smtp.gmail.com"
 port = 465
 
 # what to send
-msg = EmailMessage()
+msg = MIMEMultipart()
 msg['To'] = receivers
 msg['From'] = user
 msg["Subject"] = "Log update"
@@ -40,9 +44,25 @@ message_con = """Weekly log update\n\n"""
 # sets content
 if c == 0:
     message_con += 'No errors'
-    msg.set_content(message_con)
+    msg.attach(MIMEText(message_con, 'plain'))
 else:
-    msg.add_attachment(open("log.log", "r").read())
+    # Open PDF file in binary mode
+    for file in attachment:
+        with open(file, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {file}",
+        )
+
+        # Add attachment to message and convert message to string
+        msg.attach(part)
 
 print(message_con)
 print(msg["To"])
@@ -55,7 +75,7 @@ ctx = ssl.create_default_context()
 with smtplib.SMTP_SSL(host_server, port, context=ctx) as server:
     server.login(user, password)
     print("starting to send")
-    server.send_message(msg, to_addrs=receivers)
+    server.sendmail(user, receivers, msg.as_string())
     print("sent")
 
 # removes old logs
