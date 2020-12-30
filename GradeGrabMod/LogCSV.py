5fc3273475343cbc80d57a7935ec3942f96a5dee
Arno Claassens <63562772+arnoengineering@users.noreply.github.com>
2020-12-30 16:45:37 -0700

    home
    Signed-off-by: Arno Claassens <63562772+arnoengineering@users.noreply.github.com>

--- GradeGrabMod/LogCSV.py
+++ GradeGrabMod/LogCSV.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import os
 from pandas.errors import EmptyDataError
 from GradeGrabMod import csv_file
 
 
 def grab_g():
     try:
+        if not os.path.exists(csv_file):
+            f = open(csv_file)
+            f.close()
         grade_c = pd.read_csv(csv_file, index_col=0)  # reads jason of rand artist
         grade_dict = grade_c.to_dict(orient='index')
     except (EmptyDataError, IOError):
